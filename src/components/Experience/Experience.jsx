ac06c1600bddb966137af6a951c026cba4b8bf5f
GUchechi <godswilluchendu7@gmail.com>
2022-10-20 00:24:56 +0100

    added Experince section

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -3,7 +3,23 @@ import './Experience.css'
 
 const Experience = () => {
   return (
-    <div className="experience"></div>
+    <div className="experience">
+      <div className="achievement">
+        <div className="circle">45+</div>
+        <span>Completed </span>
+        <span>Projects</span>
+      </div>
+      <div className="achievement">
+        <div className="circle">1+</div>
+        <span>Companies</span>
+        <span>Worked</span>
+      </div>
+      <div className="achievement">
+        <div className="circle">1+</div>
+        <span>Years </span>
+        <span> Experience</span>
+      </div>
+    </div>
   )
 }
 
