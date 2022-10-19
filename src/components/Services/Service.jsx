b70bfd5163f5dbd2d03feede0b485518b05e7cd8
GUchechi <godswilluchendu7@gmail.com>
2022-10-19 14:51:25 +0100

    created a service component

---
+++ src/components/Services/Service.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Service = () => {
+  return (
+    <div className="services">
+        {/* left side */}
+        <div className="awesome">
+            
+        </div>
+
+        {/* Right side */}
+        <div className="cards">
+
+        </div>
+    </div>
+  )
+}
+
+export default Service
