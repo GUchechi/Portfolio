d2d7a6eedf9758ba2b72b1af14993fcdf3597707
GUchechi <godswilluchendu7@gmail.com>
2022-10-20 16:18:11 +0100

    added react unicons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import './Footer.css'
+import Wave from "../../img/wave.png";
+import Insta from "@iconscout/react-unicons/icons/uil-instagram";
+import Facebook from "@iconscout/react-unicons/icons/uil-facebook";
+import Gitub from "@iconscout/react-unicons/icons/uil-github";
 
 const Footer = () => {
   return (
