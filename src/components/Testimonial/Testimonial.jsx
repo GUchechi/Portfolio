719cf663dd7f2b3f5918a05dd209751e2d075c4e
GUchechi <godswilluchendu7@gmail.com>
2022-10-20 14:55:03 +0100

    added pagination

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
-import './Testimonial.css';
+import "./Testimonial.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { Pagination } from "swiper";
+import "swiper/css/pagination";
+import profilePic1 from "../../img/profile1.jpg";
+import profilePic2 from "../../img/profile2.jpg";
+import profilePic3 from "../../img/profile3.jpg";
+import profilePic4 from "../../img/profile4.jpg";
 
 const Testimonial = () => {
+
+    const clients = [
+        {
+          img: profilePic1,
+          review:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium, cumque error a nisi placeat quae exercitationem, maiores reiciendis! Eaque dicta minima, iure maiores dolorum sed.",
+        },
+        {
+          img: profilePic2,
+          review:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium, cumque error a nisi placeat quae exercitationem, maiores reiciendis! Eaque dicta minima, iure maiores dolorum sed.",
+        },
+        {
+          img: profilePic3,
+          review:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium, cumque error a nisi placeat quae exercitationem, maiores reiciendis! Eaque dicta minima, iure maiores dolorum sed.",
+        },
+        {
+          img: profilePic4,
+          review:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium, cumque error a nisi placeat quae exercitationem, maiores reiciendis! Eaque dicta minima, iure maiores dolorum sed.",
+        },
+      ];
+    
   return (
-    <div>Testimonial</div>
+   <div className="t-wrapper">
+        <div className="t-header">
+            <span>Clients always get</span>
+            <span>Exceptional work</span>
+            <span>from me...</span>
+
+            <div className="blur t-blur1" style={{background: 'var(--purple)'}}></div>
+            <div className="blur t-blur2" style={{background: 'skyblue'}}></div>
+        </div>
+        {/* Slider */}
+        <Swiper
+            modules={[Pagination]}
+            slidesPerView={1}
+            pagination={{clickable: true}}
+        >
+            {
+                clients.map((client, index) => {
+                    return (
+                        <SwiperSlide key={index}>
+                            <img src={client.img} alt="" />
+                            <span>{client.review}</span>
+                        </SwiperSlide>
+                    )
+                })
+            }
+        </Swiper>
+   </div>
   )
 }
 
