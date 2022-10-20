07b3b97df1f63898e492b421f1336fe8c89a9aed
GUchechi <godswilluchendu7@gmail.com>
2022-10-20 16:39:10 +0100

    added toggle component

--- src/components/Toggle/Toggle.jsx
+++ src/components/Toggle/Toggle.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Toggle.css'
+import Moon from "@iconscout/react-unicons/icons/uil-moon";
+import Sun from "@iconscout/react-unicons/icons/uil-sun";
 
 const Toggle = () => {
   return (
-    <div>
-      
+    <div className="toggle">
+        <Moon />
+        <Sun />
+
+        <d className="t-button"></d>
     </div>
   )
 }
